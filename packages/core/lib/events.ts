c616be9301332e2fbc3aad00c513f5be57c2eae6
tpluscode <tpluscode@users.noreply.github.com>
2019-11-18 15:40:12 +0100

    feat: use debug for logging event handlers

--- packages/core/lib/events.ts
+++ packages/core/lib/events.ts
@@ -1,6 +1,8 @@
 import { EventEmitter } from 'eventemitter3'
+import debug from 'debug'
 import { DomainEvent } from './index'
 
+const logger = debug('fun-ddr:events')
 const emitter = new EventEmitter()
 
 export interface CoreEvents {
@@ -11,9 +13,22 @@ export interface CoreEvents {
 
 type PossiblyDomainEvent<T extends Record<string, any>, K extends keyof Pick<T, string>> = unknown extends T[K] ? never : DomainEvent<T[K]>
 
-export function handle<T extends Record<string, any>, K extends keyof Pick<T, string>> (name: K, handler: (ev: PossiblyDomainEvent<T, K>) => void) {
-  console.log(`Adding handler for event ${name}: ${handler.name}`)
-  emitter.on(name, handler)
+export function handle<T extends Record<string, any>, K extends keyof Pick<T, string>> (name: K, handler: (ev: PossiblyDomainEvent<T, K>) => void | Promise<any>) {
+  logger(`Adding handler for event ${name}: ${handler.name}`)
+
+  const handlerLog = logger.extend(name)
+  emitter.on(name, async (ev: PossiblyDomainEvent<T, K>) => {
+    handlerLog('Handling started')
+
+    try {
+      await handler(ev)
+    } catch (e) {
+      handlerLog.extend('error')('Error in handler %O', e)
+      return
+    }
+
+    handlerLog('Handling finished')
+  })
 
   return (id: string, data: T[K]) => handler({
     id,
@@ -23,6 +38,6 @@ export function handle<T extends Record<string, any>, K extends keyof Pick<T, st
 }
 
 export function emit (name: string, ev: DomainEvent) {
-  console.log(`Emitting event ${ev.name} for resource ${ev.id}`)
+  logger(`Emitting event ${ev.name} for resource ${ev.id}`)
   emitter.emit(name, ev)
 }
