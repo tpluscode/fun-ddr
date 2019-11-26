c8af16aad294b640fe9cac14474f0f18d09ac7c5
tpluscode <tpluscode@users.noreply.github.com>
2019-11-26 16:42:40 +0100

    chore: added more debugging output to repository

--- packages/sparql-graph-repository/index.ts
+++ packages/sparql-graph-repository/index.ts
@@ -1,6 +1,7 @@
 import { Entity, AggregateRoot, Repository } from '@tpluscode/fun-ddr/lib'
 import { AggregateRootImpl } from '@tpluscode/fun-ddr/lib/AggregateRootImpl'
 import SparqlHttp from 'sparql-http-client'
+import debug from 'debug'
 import ParserJsonld from '@rdfjs/parser-jsonld'
 import { frame, fromRDF } from 'jsonld'
 import stringToStream from 'string-to-stream'
@@ -10,6 +11,9 @@ import uuid from 'uuid'
 import { AggregateNotFoundError, ConcurrencyError } from '@tpluscode/fun-ddr/lib/errors'
 import { JsonLdArray } from 'jsonld/jsonld-spec'
 
+const log = debug('fun-ddr:repository')
+const logError = log.extend('error')
+const logResponse = log.extend('endpoint')
 const parserJsonld = new ParserJsonld()
 
 export class SparqlGraphRepository<S extends Entity> implements Repository<S> {
@@ -19,6 +23,9 @@ export class SparqlGraphRepository<S extends Entity> implements Repository<S> {
   private readonly __frame: object
 
   public constructor (sparql: SparqlHttp, base: string, context: object, frame: object) {
+    log('Initialising SPARQL repository: %O', {
+      base,
+    })
     this.__sparql = sparql
     this.__base = base
     this.__context = context
@@ -29,8 +36,11 @@ export class SparqlGraphRepository<S extends Entity> implements Repository<S> {
     let graphUri = `urn:ddd:root:${uuid()}`
     const state = await ar.state
     if (!state) {
-      throw new Error(`Failed to save aggregate: ${await ar.error}`)
+      const error = await ar.error
+      log('Cannot save aggregate because %s', error)
+      throw new Error(`Failed to save aggregate: ${error}`)
     }
+    log('Saving aggregate %s at version %d in graph %s', state['@id'], version, graphUri)
 
     const id = state['@id']
     const jsonld = {
@@ -54,21 +64,27 @@ export class SparqlGraphRepository<S extends Entity> implements Repository<S> {
         }
       }`)
     if (!selectRootGraph.ok) {
+      log('Failed to retrieve current state of aggregate %s. Server response %s', state['@id'], await selectRootGraph.text())
+      logResponse('Response was %d: %s', selectRootGraph.status, await selectRootGraph.text())
       throw new Error(`Failed to read aggregate root: '${selectRootGraph.statusText}'`)
     }
     const json = (await selectRootGraph.json())
 
     if (json.results.bindings.length > 1) {
-      throw new Error(`Failed to save aggregate: Found multiple graphs: ${json.results.bindings.map(b => b.graph.value).join(', ')}`)
+      const graphUris = json.results.bindings.map(b => b.graph.value)
+      logError('Multiple graphs found for aggregate %s: %s', state['@id'], graphUris.join('\n'))
+      throw new Error('Failed to save aggregate: Found multiple graphs')
     } else if (json.results.bindings.length > 0) {
       const [{ graph, currentVersion }] = json.results.bindings
       if (!currentVersion) {
         if (version > 1) {
+          logError('Cannot save version %d. Previous version appears to have not number', version)
           throw new ConcurrencyError(id, 0, version)
         }
       } else {
         const currentVersionNumber = Number.parseInt(currentVersion.value)
         if (version !== currentVersionNumber + 1) {
+          logError('Cannot save version %d where %d already exists', version, currentVersionNumber)
           throw new ConcurrencyError(id, currentVersionNumber, version)
         }
       }
@@ -98,11 +114,16 @@ export class SparqlGraphRepository<S extends Entity> implements Repository<S> {
     `)
 
     if (!response.ok) {
-      throw new Error(`Failed to save aggregate root: '${response.statusText}'`)
+      logError('Failed to save aggregate root: %s', response.statusText)
+      logResponse('Response was %d: %s', response.status, await response.text())
+      throw new Error('Failed to save aggregate root')
     }
+
+    log('Aggregate root saved')
   }
 
   public async load (id: string): Promise<AggregateRoot<S>> {
+    log('Loading aggregate root %s', id)
     const graph = await this.__sparql.constructQuery(
       `BASE <${this.__base}>
     
@@ -121,7 +142,9 @@ export class SparqlGraphRepository<S extends Entity> implements Repository<S> {
     }`) as any
 
     if (!graph.ok) {
-      throw new Error(`Failed to load aggregate root: '${graph.statusText}'`)
+      logError('Failed to load aggregate root: %s', graph.statusText)
+      logResponse('Response was %d: %s', graph.status, await graph.text())
+      throw new Error('Failed to load aggregate root')
     }
 
     const dataset = await rdf.dataset().import(await graph.quadStream())
@@ -140,13 +163,16 @@ export class SparqlGraphRepository<S extends Entity> implements Repository<S> {
 
     if (state) {
       const version = jsonldArray.find(obj => obj['urn:ddd:version'])['urn:ddd:version'][0]['@value']
+      log('Loaded version %d of aggregate %s', version, id)
       return new AggregateRootImpl<S>(state, Number.parseInt(version))
     }
 
+    logError('Failed to load aggregate root. Failed to retrieve state object from graph %s', jsonld)
     return new AggregateRootImpl<S>(new AggregateNotFoundError(id))
   }
 
   public async 'delete' (id: string): Promise<void> {
+    log('Deleting aggregate root %s', id)
     const response = await this.__sparql.updateQuery(
       `BASE <${this.__base}>
       
@@ -166,7 +192,11 @@ export class SparqlGraphRepository<S extends Entity> implements Repository<S> {
     `)
 
     if (!response.ok) {
-      throw new Error(`Failed to load aggregate root: '${response.statusText}'`)
+      logError('failed to delete: %s', response.statusText)
+      logResponse('Response was %d: %s', response.status, await response.text())
+      throw new Error('Failed to load aggregate root')
     }
+
+    log('Deleted successfully')
   }
 }
